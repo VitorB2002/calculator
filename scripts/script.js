4725296776ff8ad86dbb9527796247fe06a7de32
VitorB2002 <vitorb.santos2002@gmail.com>
2022-11-04 12:17:49 -0300

    Add numeric events listener

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,60 @@
-console.log("HELLO WORLD!");
+const oneBtn = document.getElementById("one");
+const twoBtn = document.getElementById("two");
+const threeBtn = document.getElementById("three");
+const fourBtn = document.getElementById("four");
+const fiveBtn = document.getElementById("five");
+const sixBtn = document.getElementById("six");
+const sevenBtn = document.getElementById("seven");
+const eightBtn = document.getElementById("eight");
+const nineBtn = document.getElementById("nine");
+const zeroBtn = document.getElementById("zero");
+const clear = document.getElementById("clear");
+const equals = document.getElementById("equals");
+
+zeroBtn.addEventListener("click", () => {
+    console.log("0");
+    });
+
+oneBtn.addEventListener("click", () => { 
+    console.log("1");
+    });
+
+twoBtn.addEventListener("click", () => {
+     console.log("2");
+    });
+
+threeBtn.addEventListener("click", () => {
+     console.log("3");
+    });
+
+fourBtn.addEventListener("click", () => {
+     console.log("4");
+    });
+
+fiveBtn.addEventListener("click", () => {
+     console.log("5");
+    });
+
+sixBtn.addEventListener("click", () => {
+     console.log("6");
+    });
+
+sevenBtn.addEventListener("click", () => {
+     console.log("7");
+    });
+
+eightBtn.addEventListener("click", () => {
+     console.log("8");
+    });
+
+nineBtn.addEventListener("click", () => {
+     console.log("9");
+    });
+
+clear.addEventListener("click", () => {
+    console.log("clear");
+    });
+
+equals.addEventListener("click", () => {
+    console.log("equals");
+    });
