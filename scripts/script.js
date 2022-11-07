bf12561fc2fd020cf24b83d1c5520859507ed335
VitorB2002 <vitorb.santos2002@gmail.com>
2022-11-07 13:01:17 -0300

    Add operators events

--- scripts/script.js
+++ scripts/script.js
@@ -11,66 +11,92 @@ const nineBtn = document.getElementById("nine");
 const zeroBtn = document.getElementById("zero");
 const clear = document.getElementById("clear");
 const equals = document.getElementById("equals");
+const sumBtn = document.getElementById("sum");
+const subBtn = document.getElementById("sub");
+const mulBtn = document.getElementById("mul");
+const divBtn = document.getElementById("div");
+
 let content = "";
 
 function addDisplayContent(content){
     display.innerHTML = content;
 }
 
+//Numeric keyboard events
+
 zeroBtn.addEventListener("click", () => {
-        content += "0";
-        addDisplayContent(content);
-    });
+    content += "0";
+    addDisplayContent(content);
+});
 
 oneBtn.addEventListener("click", () => { 
-        content += "1";
-        addDisplayContent(content);
-    });
+    content += "1";
+    addDisplayContent(content);
+});
 
 twoBtn.addEventListener("click", () => {
-        content += "2";
-        addDisplayContent(content);
-    });
+    content += "2";
+    addDisplayContent(content);
+});
 
 threeBtn.addEventListener("click", () => {
-        content += "3";
-        addDisplayContent(content);
-    });
+    content += "3";
+    addDisplayContent(content);
+});
 
 fourBtn.addEventListener("click", () => {
-        content += "4";
-        addDisplayContent(content);
-    });
+    content += "4";
+    addDisplayContent(content);
+});
 
 fiveBtn.addEventListener("click", () => {
-        content += "5";
-        addDisplayContent(content);
-    });
+    content += "5";
+    addDisplayContent(content);
+});
 
 sixBtn.addEventListener("click", () => {
-        content += "6";
-        addDisplayContent(content);
-    });
+    content += "6";
+    addDisplayContent(content);
+});
 
 sevenBtn.addEventListener("click", () => {
-        content += "7";
-        addDisplayContent(content);
-    });
+    content += "7";
+    addDisplayContent(content);
+});
 
 eightBtn.addEventListener("click", () => {
         content += "8";
         addDisplayContent(content);
-    });
+});
 
 nineBtn.addEventListener("click", () => {
-        content += "9";
-        addDisplayContent(content);
-    });
+    content += "9";
+    addDisplayContent(content);
+});
 
 clear.addEventListener("click", () => {
-        content = "";
-        addDisplayContent(content);
-    });
+    content = "";
+    addDisplayContent(content);
+});
 
 equals.addEventListener("click", () => {
-    });
+});
+
+//Operators keyboard events
+
+sumBtn.addEventListener("click", () => {
+    content += "+";
+    addDisplayContent(content);
+});
+subBtn.addEventListener("click", () => {
+    content += "-";
+    addDisplayContent(content);
+});
+mulBtn.addEventListener("click", () => {
+    content += "*";
+    addDisplayContent(content);
+});
+divBtn.addEventListener("click", () => {
+    content += "/";
+    addDisplayContent(content);
+});
